d23a3e40ab6a9b7807305edb913575b026bf03f3
Jerryc6 <jerryclanuevo27@gmail.com>
2025-11-18 09:53:28 -0500

    additonal knowledge

---
+++ ex_argu.java
@@ -0,0 +1,17 @@
+public class ex_argu{
+
+	public static int multiply(int a, int b){
+		return a * b;
+	
+	}
+
+	public static void main(String[] args){
+		int x = 2;
+		int y = 5;
+
+		int product = multiply(x, y);
+		System.out.println("PRODUCT IS: " + product);
+	}
+
+
+}
